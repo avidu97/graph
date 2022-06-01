43099e2963bbdff86f55f96d262e0e7f744074e0
Avidu <avidu97@gmail.com>
2022-06-01 11:06:48 +0530

    mani image and title basics completed

--- src/pages/projects/[slug].js
+++ src/pages/projects/[slug].js
@@ -66,54 +66,65 @@ export default ({ project }) => (
       <div className={styles.main_container}>
         {/* ______________________main image________________________________ */}
 
-        <div></div>
-        <div>
-          <img src={project.mainImage.url} alt={project.title} />
+        <div className={styles.main_image_container}>
+          <div>
+            <img className={styles.main_image} src={project.mainImage.url} alt={project.title} />
+          </div>
         </div>
+        
 
         {/* ______________________title and description_____________________ */}
-
-        <div className={styles.t_d_holder}>
-          <div className={styles.title_holder}>
-            <h1>{project.title}</h1>
-          </div>
-          <div className={styles.des_holder}>
-            <p>{project.description}</p>
+        
+        <div className={styles.title_container}>
+          <div className={styles.t_d_holder}>
+            <div className={styles.title_holder}>
+              <h1>{project.title}</h1>
+            </div>
+            <div className={styles.des_holder}>
+              <p>{project.description}</p>
+            </div>
           </div>
         </div>
+        
+
+        <div className={styles.hr}></div>
 
         {/* ________________________tag area________________________________ */}
 
         <div className={styles.tag_container}>
-          <div className={styles.tag_wrapper}>
-            <div className={styles.tag_single}>
-              <p>
-                Date: <span className={styles.tag}>{project.date}</span>
-              </p>
-            </div>
-            <div className={styles.tag_single}>
-              <p>
-                Platform: <span className={styles.tag}>{project.platform}</span>
-              </p>
-            </div>
-            <div className={styles.tag_single}>
-              <p>
-                Company: <span className={styles.tag}>{project.company}</span>
-              </p>
-            </div>
-            <div className={styles.tag_single}>
-              <p>
-                Demo URL: <span className={styles.tag}>{project.demoUrl}</span>
-              </p>
-            </div>
-            <div className={styles.tag_single}>
-              <p>
-                Source URL:{" "}
-                <span className={styles.tag}>{project.sourceUrl}</span>
-              </p>
+          <div className={styles.tag_holder}>
+            <div className={styles.tag_wrapper}>
+              <div className={styles.tag_single}>
+                <p>
+                  Date: <span className={styles.tag}>{project.date}</span>
+                </p>
+              </div>
+              <div className={styles.tag_single}>
+                <p>
+                  Platform: <span className={styles.tag}>{project.platform}</span>
+                </p>
+              </div>
+              <div className={styles.tag_single}>
+                <p>
+                  Company: <span className={styles.tag}>{project.company}</span>
+                </p>
+              </div>
+              <div className={styles.tag_single}>
+                <p>
+                  Demo URL: <span className={styles.tag}>{project.demoUrl}</span>
+                </p>
+              </div>
+              <div className={styles.tag_single}>
+                <p>
+                  Source URL:{" "}
+                  <span className={styles.tag}>{project.sourceUrl}</span>
+                </p>
+              </div>
             </div>
           </div>
         </div>
+        
+        <div className={styles.hr}></div>
 
         {/* ______________________content area______________________________ */}
 
@@ -124,6 +135,8 @@ export default ({ project }) => (
             />
           </div>
         </div>
+
+        <div className={styles.hr}></div>
 
         {/* __________________________footer________________________________ */}
 
