b2f64d084288a2cc30cd6e3feaf780c5bf5d0caf
Avidu <avidu97@gmail.com>
2022-06-01 10:44:00 +0530

    project page content structured

--- src/pages/projects/[slug].js
+++ src/pages/projects/[slug].js
@@ -1,10 +1,8 @@
-import { GraphQLClient } from 'graphql-request';
-import styles from "../../../styles/projects.module.css"
-
-
+import { GraphQLClient } from "graphql-request";
+import styles from "../../../styles/projects.module.css";
 
 const graphcms = new GraphQLClient(
-  'https://api-ap-south-1.graphcms.com/v2/cl3rbzoet8srg01xk3ialfs6j/master'
+  "https://api-ap-south-1.graphcms.com/v2/cl3rbzoet8srg01xk3ialfs6j/master"
 );
 
 export async function getStaticProps({ params }) {
@@ -64,15 +62,75 @@ export async function getStaticPaths() {
 
 export default ({ project }) => (
   <React.Fragment>
-    <img src={project.mainImage.url} alt={project.title} />
-    <h1>{project.title}</h1>
-    <p>{project.description}</p>
-    <p>{project.sourceUrl}</p>
-    <p>{project.demoUrl}</p>
-    <p>{project.platform}</p>
-    <p>{project.company}</p>
-    <p>{project.date}</p>
-    <div dangerouslySetInnerHTML={{__html: project.projectContent.html}} />
-    <img src={project.projectImages.url} alt={project.projectImages.fileName} />
+    <div className={styles.main}>
+      <div className={styles.main_container}>
+        {/* ______________________main image________________________________ */}
+
+        <div></div>
+        <div>
+          <img src={project.mainImage.url} alt={project.title} />
+        </div>
+
+        {/* ______________________title and description_____________________ */}
+
+        <div className={styles.t_d_holder}>
+          <div className={styles.title_holder}>
+            <h1>{project.title}</h1>
+          </div>
+          <div className={styles.des_holder}>
+            <p>{project.description}</p>
+          </div>
+        </div>
+
+        {/* ________________________tag area________________________________ */}
+
+        <div className={styles.tag_container}>
+          <div className={styles.tag_wrapper}>
+            <div className={styles.tag_single}>
+              <p>
+                Date: <span className={styles.tag}>{project.date}</span>
+              </p>
+            </div>
+            <div className={styles.tag_single}>
+              <p>
+                Platform: <span className={styles.tag}>{project.platform}</span>
+              </p>
+            </div>
+            <div className={styles.tag_single}>
+              <p>
+                Company: <span className={styles.tag}>{project.company}</span>
+              </p>
+            </div>
+            <div className={styles.tag_single}>
+              <p>
+                Demo URL: <span className={styles.tag}>{project.demoUrl}</span>
+              </p>
+            </div>
+            <div className={styles.tag_single}>
+              <p>
+                Source URL:{" "}
+                <span className={styles.tag}>{project.sourceUrl}</span>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* ______________________content area______________________________ */}
+
+        <div>
+          <div>
+            <div
+              dangerouslySetInnerHTML={{ __html: project.projectContent.html }}
+            />
+          </div>
+        </div>
+
+        {/* __________________________footer________________________________ */}
+
+        <footer>
+          <div className={styles.footer}>avidu©2022</div>
+        </footer>
+      </div>
+    </div>
   </React.Fragment>
 );
